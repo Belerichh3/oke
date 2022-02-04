4b3b3615bd48766831fca8fb6b7b25e0f326121e
Glitch (butternut-courageous-guide) <none>
2022-02-04 01:35:48 +0000

    💪🔋 Checkpoint ./main.js:25915949/1237 ./config.js:25915949/800

--- main.js
+++ main.js
@@ -37,16 +37,3 @@ console.log("Projene Girdiğin Bot Tokeni Hatalı Veya Botunun Intentleri KAPALI
 } else {
 console.log("Projenizde Bulunan .env İsimli Dosyadaki TOKEN Yazısının Karşısına Lütfen Bot Tokeninizi Yazın!")
 }
-
-client.on("ready", () => {
-       const channel = client.channels.cache.get('938965651886854185');
-    if (!channel) return console.error("The channel does not exist!");
-    channel.join().then(connection => {
-        // Yay, it worked!
-        console.log("Successfully connected.");
-    }).catch(e => {
-
-        // Oh no, it errored! Let's log it to console :)
-        console.error(e);
-    });
-});
